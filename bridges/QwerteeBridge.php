58b35ad2dfa5de0fd47634b051824c07c6606d5a
kadogo <kadogo@users.noreply.github.com>
2022-07-09 20:43:37 +0200

    Break after 3 tees items

--- bridges/QwerteeBridge.php
+++ bridges/QwerteeBridge.php
@@ -11,15 +11,20 @@ class QwerteeBridge extends BridgeAbstract
     public function collectData()
     {
         $html = getSimpleHTMLDOM(self::URI);
-        $content = $html->find('.big-slides');
+        $count = 0
 
-        foreach ($content->find('.index-tee') as $element) {
+        foreach ($content->find('.big-slides .index-tee') as $element) {
             $item = [];
             $item['uri']      = $element->find('img')->src;
             $item['title']    = $element->find('.title > span')->innertext;
             $item['author']   = $element->find('.title > a')->innertext;
             $item['content']  = $element->find('img');
             $this->items[]    = $item;
+
+            // Break if we have 3 elements because we doesn't want the last chance tees
+            if ($this->items.length == 3) {
+              break;
+            }
         }
     }
 }
